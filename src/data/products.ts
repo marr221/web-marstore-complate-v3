43b20d8ca2c709cb093db97c66f7c1068d933718
mar <mmmmaaarrr925@gmail.com>
2026-07-27 07:28:23 +0700

    updt

--- src/data/products.ts
+++ src/data/products.ts
@@ -32,22 +32,4 @@ export const PRODUCTS: Product[] = [
       'info' : ' jual akun langsung ke wa aja'
     }
   },
-  {
-    id: 39,
-    title: 'Akun FreeFire Murah',
-    game: 'Free Fire',
-    price: 170000,
-    oldPrice: 200000,
-    rating: 5,
-    seller: 'MARSTORE',
-    image: 'https://i.ibb.co.com/xtQsQ1ZD/IMG-20260720-WA0517.jpg',
-    verified: true,
-    specs: {
-      'Info' : ' Harga akun bisa dinego',
-      'Vault' : '380+',
-      'Evogun' : 'XM8 level max',
-      'Shotgun 2' : 'Gurun pasir',
-      'info2' : 'Info lebih lengkap chat admin saja'
-    }
-  },
 ];
