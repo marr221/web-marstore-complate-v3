84f2a9f918242c6871a5910dbfb4f753cada5dfa
mar <mmmmaaarrr925@gmail.com>
2026-08-16 19:35:55 +0700

    40

--- src/data/products.ts
+++ src/data/products.ts
@@ -29,7 +29,25 @@ export const PRODUCTS: Product[] = [
     image: 'https://i.ibb.co.com/ymGFh9gf/In-Collage-20260719-223644467.jpg',
     verified: true,
     specs: {
-      'info' : ' jual akun langsung ke wa aja'
+      'info' : ' jual akun langsung ke wa aja',
+    },
+  },
+  {
+    id: 40,
+    title: 'Stok Akun ML Kolektor Terhormat',
+    game: 'Mobuile Legends',
+    price: 550.000,
+    oldPrice: 650.000,
+    rating: 5,
+    seller: 'MARSTORE',
+    image: 'https://i.ibb.co.com/QLdcDtJ/Screenshot-2026-08-13-20-44-20-455-com-mobilelegends-mi.jpg',
+    verified: true,
+    specs: {
+      'Keterangan' : 'Kolektor terhormat 5',
+      'Skin legend' : 'Alucard Legend',
+      'Skin Grand' : 'Karina KOF & Hayabusa Exorcist',
+      'Skin Epic' : 'Bejibun',
+      'Info Harga' : 'harga bisa dinego'
     }
   },
 ];
